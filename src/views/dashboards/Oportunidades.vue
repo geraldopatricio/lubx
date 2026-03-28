340e0af7e9e830a5f4866d02f20dbfd94997ce6c
Geraldo P Melo <gpatricio.melo@gmail.com>
2026-03-28 20:03:35 -0300

    Add Power BI iframe to Oportunidades dashboard

--- src/views/dashboards/Oportunidades.vue
+++ src/views/dashboards/Oportunidades.vue
@@ -35,6 +35,8 @@ const handleLoad = () => {
           <span class="mt-2 fw-bold text-muted">Carregando Relatório...</span>
         </div>
 
+        <iframe title="BI Oportunidades" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiMzM5NWYxZmEtMmFkYS00MjE1LTlmZWYtZTI5NGY4ZjA5ZmUyIiwidCI6ImEzYTY5ODI1LTA2YTMtNDU0Ny1iZGZkLTBlYWI3MDJmMTcyNiJ9" frameborder="0" allowFullScreen="true"></iframe>
+
       </div>
     </div>
   </div>
